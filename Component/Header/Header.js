ce8726ecc16b9eb2b1c91bb4c0f044ce2fcd2122
khuephung <khuephung.31211021133@st.ueh.edu.vn>
2023-12-19 19:08:24 +0700

    final

--- Component/Header/Header.js
+++ Component/Header/Header.js
@@ -1,13 +1,18 @@
+// Lấy ra phần tử HTML có id là 'header-open' và lưu trữ nó trong biến headerOpen.
 const headerOpen = document.querySelector('#header-open')
+//  Lấy ra phần tử HTML có id là 'header-close' và lưu trữ nó trong biến headerClose.
 const headerClose = document.querySelector('#header-close')
+// Lấy ra phần tử HTML có lớp là 'menu-mobile' và lưu trữ nó trong biến mobileMenu.
 const mobileMenu = document.querySelector('.menu-mobile')
 
 
-
+//  Khi phần tử với id 'header-open' được click một lớp CSS
+// được thêm vào phần tử có lớp là 'menu-mobile' -> hiện thị phần tử
 headerOpen.onclick = () => {
     mobileMenu.classList.add('open')
 }
 
+//ẩn đi phần tử.
 headerClose.onclick = () => {
     mobileMenu.classList.remove('open')
 }
