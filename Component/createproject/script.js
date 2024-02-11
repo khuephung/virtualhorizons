2128069262d217b624e6525f157a48b7d8e39979
khuephung <khuephung.31211021133@st.ueh.edu.vn>
2024-02-11 20:51:54 +0700

    change to fix createproject

--- Component/createproject/script.js
+++ Component/createproject/script.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper(".slide-content", {
+var swiper = new Swiper(".slide-content-laptop", {
     slidesPerView: 3,
     spaceBetween: 25,
     loop: true,
@@ -25,5 +25,56 @@ var swiper = new Swiper(".slide-content", {
         950: {
             slidesPerView: 3,
         },
+    },
+  });
+
+  var swiper_tablet = new Swiper(".slide-content-tablet", {
+    slidesPerView: 2,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        }
+    },
+  });
+
+  var swiper_phone = new Swiper(".slide-content-phone", {
+    slidesPerView: 1,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        }
     },
   });
